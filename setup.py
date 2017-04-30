565ba97770281b19b6c17a68f2d970a595545986
Kelsey McDonald <krm58@duke.edu>
2017-04-30 09:56:30 -0400

    add file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'SSVD_Biclustering',
+    packages = ['stats_SSVD_package'],
+    version = '1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'test release',
+    author = 'Kelsey McDonald and Cecily Abraham',
+    author_email = 'krm58@duke.edu',
+    url = 'https://github.com/krm58/Biclustering-SSVD',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
